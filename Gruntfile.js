81c2ae867a2b8118f0d2f592de24a8939c49113a
sarapiette <spiette@thoughtworks.com>
2016-11-19 18:08:10 -0500

    updated grunt file to open proper server

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,44 +7,19 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      autoprefixer: {
-        files: ['<%= app/css/*.css'],
-        tasks: ['copy:stageCss', 'autoprefixer:server']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-      }
-    },
     connect: {
-      options: {
-        port: 8080,
-        livereload: 1337,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          open: true,
-          base: [
-            'app/*.html'
-          ]
+      server: {
+        options:{
+          port: 9000,
+          base: 'app',
+          keepalive: true,
+          open:{
+            target: 'http://localhost:9000'
+          }
         }
-      }
-    },
-    autoprefixer: {
-      options: {
-          browsers: ['> 5%', 'last 10 versions', 'Firefox ESR', 'Opera 12.1']
       }
     }
 
   });
-
-
-  // Default task(s).
-  grunt.registerTask('serve', ['connect:livereload','watch']);
 
 };
