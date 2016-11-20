2c8bd59d7aaae020c034e8eaff832aad374d82d6
kholibri3 <ho22k@mtholyoke.edu>
2016-11-20 09:47:50 -0500

    added South American drum artifact to blank painting on wall and all over box/cube

--- src/app/js/src/scene.js
+++ src/app/js/src/scene.js
@@ -17,18 +17,30 @@ class Scene{
         // create a basic light, aiming 0,1,0 - meaning, to the sky
         const light = new babylon.HemisphericLight('light1', new babylon.Vector3(3,1,0), scene);
         const light2 = new babylon.HemisphericLight('light2', new babylon.Vector3(0,1,3), scene);
+        const light3 = new babylon.HemisphericLight('light3', new babylon.Vector3(3,1,-10), scene);
         light.intensity = 0.7;
+        light3.intensity = 0.5;
 
         //create box
         const box = babylon.Mesh.CreateBox("box", 4.0, scene, false, babylon.Mesh.DEFAULTSIDE);
         box.position = new babylon.Vector3(-4,2,-4);
 
+        const drumBox = new babylon.StandardMaterial('South American drum', scene);
+        const drumTexture = new babylon.Texture("images/southAmericanDrum.png",scene);
+        drumBox.diffuseTexture = drumTexture;
+        box.material = drumBox;
+
         const knot = babylon.Mesh.CreateTorusKnot("knot", 2, 0.5, 128, 64, 2, 3, scene);
-        knot.position = new babylon.Vector3(5,3,5);
+        knot.position = new babylon.Vector3(-10,3,5);
 
         // const painting = babylon.Mesh.CreatePlane("painting", 5.0, 4.0, scene, false, babylon.Mesh.DOUBLESIDE);
         const painting = this.createWall("painting", 5.25, 4.0, scene);
         painting.position = new babylon.Vector3(6.3,10.5,9);
+
+        const drumPainting = new babylon.StandardMaterial('South American drum', scene);
+        const drumFlatTexture = new babylon.Texture("images/southAmericanDrum.jpg",scene);
+        drumPainting.diffuseTexture = drumFlatTexture;
+        painting.material = drumPainting;
 
 
         var tiledGround = new babylon.Mesh.CreateGround("Tiled Ground", 40, 20, 1, scene);
@@ -54,15 +66,15 @@ class Scene{
         leftWall.position = new babylon.Vector3(-20,10,0);
         leftWall.rotation.y = Math.PI/2;
 
-        const textureWall = new babylon.StandardMaterial("texture1", scene);
-        textureWall.diffuseTexture = new babylon.Texture("images/museum-wall.jpg", scene);
-        textureWall.diffuseTexture.uOffset = 1.5;
-        textureWall.diffuseTexture.vOffset = 5.0;
-        textureWall.backFaceCulling = false;
+        const museumWallpaper = new babylon.StandardMaterial("museumWallpaper", scene);
+        museumWallpaper.diffuseTexture = new babylon.Texture("images/museum-wall.jpg", scene);
+        museumWallpaper.diffuseTexture.uOffset = 1.5;
+        museumWallpaper.diffuseTexture.vOffset = 5.0;
+        museumWallpaper.backFaceCulling = false;
 
-        rightWall.material = textureWall;
-        leftWall.material = textureWall;
-        backWall.material = textureWall;
+        rightWall.material = museumWallpaper;
+        leftWall.material = museumWallpaper;
+        backWall.material = museumWallpaper;
 
         // return the created scene
         return scene;
